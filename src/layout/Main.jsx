7351e2dcc4f64ad2d6b180bb93b87f189b49e3bc
ihsajjad <sajjadmeal@gmail.com>
2023-05-18 14:39:47 +0600

    added footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../shared/footer/Footer";
+import Navbar from "../shared/navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,11 +1,10 @@
-import Navbar from "../../../shared/navbar/Navbar";
 
 
 const Home = () => {
     return (
         <main>
-            <Navbar />
             <h2>This is home</h2>
+
         </main>
     );
 };
