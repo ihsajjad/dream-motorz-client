0ed914cab95a3c02c82640b011be3079819849fb
ihsajjad <sajjadmeal@gmail.com>
2023-05-18 17:30:36 +0600

    created firebase config file and .env.local file

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import router from './routes/Router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='lg:mx-20 mx-2 lg:mt-4 border-2 border-purple-700 shadow-xl'>
+    <div className='lg:mx-20 mx-2 lg:mt-4 shadow-2xl'>
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>,
--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -9,13 +9,13 @@ const Home = () => {
                         <img src="https://shorturl.at/hjTVY" className="h-full w-full"/>
                     </div>
                     <div id="item2" className="carousel-item w-full h-screen">
-                        <img src="https://images.unsplash.com/photo-1587839871379-2d6c28071f45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=918&q=80" className="h-full w-full" />
+                        <img src="https://shorturl.at/crEJY" className="h-full w-full" />
                     </div>
                     <div id="item3" className="h-screen carousel-item w-full">
-                        <img src="https://images.unsplash.com/photo-1565174408946-a7a8c16a3933?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1031&q=80" className="h-full w-full" />
+                        <img src="https://shorturl.at/agzMV" className="h-full w-full" />
                     </div>
                     <div id="item4" className="h-screen carousel-item w-full">
-                        <img src="https://images.unsplash.com/photo-1511415518647-9e5da4fd803f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" className="h-full w-full" />
+                        <img src="https://shorturl.at/CJUW2" className="h-full w-full" />
                     </div>
                 </div>
                 <div className="flex justify-center w-full py-2 gap-2 absolute bottom-0">
