2f74731aa1d3111d9c253d175d0a95f32e3a9f0f
ihsajjad <sajjadmeal@gmail.com>
2023-05-18 17:40:53 +0600

    created authprovider.jsx file and auth context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Router.jsx'
+import AuthProvider from './providers/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='lg:mx-20 mx-2 lg:mt-4 shadow-2xl'>
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className='lg:mx-20 mx-2 lg:mt-4 shadow-2xl'>
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>,
 )
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+import app from "../firebase/firebase.config";
+import {GoogleAuthProvider, getAuth, signInWithPopup} from "firebase/auth";
+
+const auth = getAuth(app)
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+    
+    // Sign in with google
+    // const googleProvider = new GoogleAuthProvider();
+    // const googleSignIn = () => {
+    //     return signInWithPopup(auth, googleProvider)
+    // }
+    const authInfo ={
+        // googleSignIn
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
