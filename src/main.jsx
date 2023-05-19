c4f840b63451bea0704ab13178c1dcef512b5e4b
ihsajjad <sajjadmeal@gmail.com>
2023-05-19 09:50:11 +0600

    successfully added post method to upload data to databse

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import AuthProvider from './providers/AuthProvider'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <div className='lg:mx-20 mx-2 p-2 lg:mt-4 shadow-2xl'>
+      <div className='lg:mx-20 mx-2 lg:mt-4 shadow-2xl'>
         <RouterProvider router={router} />
       </div>
     </AuthProvider>
--- src/pages/addAToy/AddAToy.jsx
+++ src/pages/addAToy/AddAToy.jsx
@@ -4,30 +4,61 @@ import Spinner from "../../shared/spinner/Spinner";
 
 
 const AddAToy = () => {
-    const {user, loading} = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
 
-    if(loading){
+    if (loading) {
         return <Spinner />
     }
-    const {displayName, email} = user;
-    console.log(displayName, email);
+    const { displayName, email } = user;
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Process the form data here
-        
+        const form = e.target;
+        const photo = form.photo.value;
+        const toyName = form.toyName.value;
+        const sellerName = form.sellerName.value;
+        const sellerEmail = form.sellerEmail.value;
+        const subCategory = form.subCategory.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const availableQuantity = form.availableQuantity.value;
+        const toyDescription = form.toyDescription.value;
+
+        const newToy = {
+            photo,
+            toyName,
+            sellerName,
+            sellerEmail,
+            subCategory,
+            price,
+            rating,
+            availableQuantity,
+            toyDescription
+        };
+
+        console.log(newToy);
+
+        fetch('http://localhost:5000/products',{
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newToy)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
     };
 
     return (
         <div className="py-10">
             <form onSubmit={handleSubmit} className="md:w-3/4 mx-auto border-4 border-purple-400 border-dashed p-5 py-10 rounded-lg relative">
-            <h2 className="text-3xl font-bold text-purple-700 bg-white text-center absolute -top-5 md:left-1/3 left-1/4 px-1">Add A New Toy</h2>
+                <h2 className="text-3xl font-bold text-purple-700 bg-white text-center absolute -top-5 md:left-1/3 left-1/4 px-1">Add A New Toy</h2>
                 <div className="grid md:grid-cols-2 md:gap-5">
                     <label className="custom-label">
                         Picture URL of the toy:
                         <input
                             type="url"
-                            name="pictureUrl"
+                            name="photo"
                             placeholder="Photo URL"
                             className="custom-input"
                         />
@@ -37,7 +68,7 @@ const AddAToy = () => {
                         Toy&apos;s Name:
                         <input
                             type="text"
-                            name="name"
+                            name="toyName"
                             placeholder="Toy's Name"
                             className="custom-input"
                         />
@@ -65,11 +96,11 @@ const AddAToy = () => {
 
                     <label className="custom-label">
                         Sub-category:
-                       <select name="sub-category" id="" className="custom-input">
+                        <select name="subCategory" id="" className="custom-input">
                             <option value="car">Car</option>
                             <option value="excubator">Excubator</option>
                             <option value="osuk">Osuk</option>
-                       </select>
+                        </select>
                     </label>
 
                     <label className="custom-label">
@@ -89,6 +120,7 @@ const AddAToy = () => {
                             name="rating"
                             placeholder="Rating"
                             className="custom-input"
+                            max="5"
                         />
                     </label>
 
@@ -104,20 +136,20 @@ const AddAToy = () => {
 
                 </div>
                 <label className="custom-label">
-                    Detail Description:
+                    Toy Description:
                     <textarea
-                        name="detailDescription"
+                        name="toyDescription"
                         className="custom-input"
                         placeholder="Write something about your toy..."
                     ></textarea>
                 </label>
 
                 <div className="text-center">
-                <input
-                    type="submit"
-                    className="custom-btn"
-                    value="Submit"
-                />
+                    <input
+                        type="submit"
+                        className="custom-btn"
+                        value="Submit"
+                    />
                 </div>
             </form>
         </div>
